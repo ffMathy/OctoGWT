23921e597bcc768bbb618afed7f1cec43f4ae8af
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2015-03-02 20:34:28 +0100

    updated names of assemblies.

--- src/OctoGWT.Tests/Properties/AssemblyInfo.cs
+++ src/OctoGWT.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GivenWhenThen.Tests")]
+[assembly: AssemblyTitle("OctoGWT.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GivenWhenThen.Tests")]
+[assembly: AssemblyProduct("OctoGWT.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
